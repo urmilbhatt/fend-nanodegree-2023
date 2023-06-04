a7c4d2e1052e72723d633f96a4063c6f59426ed2
Urmil Bhatt <16276332+urmilbhatt@users.noreply.github.com>
2023-06-04 22:25:55 +0000

    Add Readme file to the project.

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,50 +2,51 @@
 projectData = {};
 
 // Require Express to run server and routes
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
 // Setup Server
-const port = 8000
+const port = 8000;
 
 // Spin up the server
-app.listen(port, listening);
+const server = app.listen(port, listening);
 
 // Callback to debug
 function listening() {
-    console.log('server is running');
-    console.log(`running on http://localhost:${port}`);
+  console.log("server is running");
+  console.log(`running on http://localhost:${port}`);
 }
 
 // GET route
-app.get('/all', sendWeather);
+app.get("/all", sendWeather);
 
 // Callback function to complete GET '/all'
 function sendWeather(req, res) {
-    res.send(projectData);
+  res.send(projectData);
 }
 
 // Post Route
-app.post('/addWeather', addWeather);
+app.post("/addWeather", addWeather);
 
 // Callback function to complete POST '/addWeather'
 function addWeather(req, res) {
-    projectData.date = req.body.date;
-    projectData.temp = req.body.temp;
-    projectData.feelings = req.body.feelings;
+  projectData.date = req.body.date;
+  projectData.temp = req.body.temp;
+  projectData.feelings = req.body.feelings;
+  // console.log(projectData);
 }
