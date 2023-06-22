407a920338f59a069afdab45041e345b97e0f7a0
Urmil Bhatt <16276332+urmilbhatt@users.noreply.github.com>
2023-06-22 18:27:01 +0000

    Add checkForText function in client js, add handleSubmit Logic in the formHandler file, add form style in css.

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,16 +1,30 @@
 function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let formText = document.getElementById('text').value;
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    // If no text is entered
+    if (Client.checkForText(formText)) {
+        alert("Please enter text before pressing submit button");
+        return;
+    };
+
+    fetch('http://localhost:8081/analysis', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({formText: formText})
+    })
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('agreement').innerHTML = res.agreement;
+        document.getElementById('subjectivity').innerHTML = res.subjectivity;
+        document.getElementById('confidence').innerHTML = res.confidence;
+        document.getElementById('irony').innerHTML = res.irony;
     })
 }
 
-export { handleSubmit }
+export { handleSubmit };
